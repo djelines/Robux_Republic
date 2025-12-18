99f664a21b0511f0f5b15b0bdc85d9339c5e2ed4
Mateis B <mateisbourlet9@gmail.com>
2025-12-18 10:31:59 +0100

    try fix backend

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -e
 
-mkdir -p /app/data
+mkdir -p /app/sql_data
+chmod 777 /app/sql_data
+
+touch /app/sql_data/database.db
+chmod 666 /app/sql_data/database.db
+
 
 python -m app.schelduler.finalize_transaction &
-
 python -m app.schelduler.mail_send &
 
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
+
+uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
