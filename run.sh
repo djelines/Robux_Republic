4b9a751da1c8d923d564a2421a98fd83b1733e36
Mateis B <mateisbourlet9@gmail.com>
2025-12-18 14:53:09 +0100

    try fix finalize

--- run.sh
+++ run.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# On s'assure que le dossier existe
+# Create sql_data directory with appropriate permissions
 mkdir -p /tmp/sql_data
 chmod 777 /tmp/sql_data
-
-# On définit le chemin pour que Python trouve le module 'app'
+# Set PYTHONPATH environment variable
 export PYTHONPATH=$PYTHONPATH:/app
 
-echo "🚀 Lancement des Schedulers en arrière-plan..."
+# Start schedulers in the background
+python -m app.schelduler.finalize_transaction & 
+python -m app.schelduler.mail_send & 
+python -m app.schelduler.process_ceiling_account &
 
-# LE "&" EST CRUCIAL ICI :
-python -m app.schelduler.finalize_transaction 
-
-
-echo "✅ Schedulers lancés. Démarrage de l'API..."
-
-# Pas de "&" pour uvicorn car c'est le processus principal qui doit maintenir le container en vie
+# Start the FastAPI application with Uvicorn
 uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
