6b37d404e776116bce6885b23daed6be52bae8ae
Mateis B <mateisbourlet9@gmail.com>
2025-12-18 14:25:25 +0100

    try fix finalize

--- app/settings/database.py
+++ app/settings/database.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, create_engine, Session
+from sqlalchemy import event
 from app.settings.config import DB_NAME
- 
- 
+
 if DB_NAME.startswith("sqlite:///"):
     sqlite_url = DB_NAME
 else:
@@ -10,6 +10,12 @@ else:
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 
+@event.listens_for(engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA journal_mode=WAL")
+    cursor.execute("PRAGMA synchronous=NORMAL")
+    cursor.close()
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -17,6 +23,3 @@ def create_db_and_tables():
 def get_session():
     with Session(engine) as session:
         yield session
-        
-
-        
