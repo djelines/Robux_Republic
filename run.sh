6250a7aaed1fd9ee8dd5c8e9b112a87dfd7636d4
Mateis B <mateisbourlet9@gmail.com>
2025-12-18 10:50:14 +0100

    fix sql_data

--- run.sh
+++ run.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -e
 
-mkdir -p /app/sql_data
-chmod 777 /app/sql_data
-
-touch /app/sql_data/database.db
-chmod 666 /app/sql_data/database.db
-
+mkdir -p /tmp/sql_data
+chmod 777 /tmp/sql_data
 
 python -m app.schelduler.finalize_transaction &
 python -m app.schelduler.mail_send &
-
 
 uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
